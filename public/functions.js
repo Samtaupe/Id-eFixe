f3c3eebec8d0ead86f1d181c130991ef5559e6fe
Corentin <clashcocoytb@gmail.com>
2024-06-16 20:25:32 +0200

    #8 - affiche l'image selectionnee + appel api non fonctionnel

--- public/functions.js
+++ public/functions.js
@@ -1,7 +1,6 @@
 var apiUrl = "http://localhost:8080/api/";
-var token = "";
-var selectedImage;
 
+/** Appel l'api pour récupérer un token et le stocker dans le localstorage */
 async function connexion() {
     let login = document.getElementById("username").value;
     let password = document.getElementById("password").value;
@@ -21,7 +20,7 @@ async function connexion() {
 
     let responseContent = await response.json();
     if (responseContent.token) {
-        token = responseContent.token;
+        localStorage.setItem('token', responseContent.token);
         let printResponse = document.getElementById('responseConnexion');
         printResponse.style.color = "green";
         printResponse.textContent = "Connexion réussie";
@@ -32,18 +31,54 @@ async function connexion() {
     }
 }
 
+/** ouvre l'explorateur pour choisir un fichier et le stocke dans le local storage */ 
 function chooseImage() {
     var input = document.createElement('input');
     input.type = 'file';
+    input.accept = '.png, .jpg, .jpeg';
     input.click();
-    input.addEventListener("change", function(e) {
+    input.addEventListener("change", async function(e) {
         if (e.target.files[0]) {
-            selectedImage = e.target.files[0];
+            const file = e.target.files[0];
+
+            const reader = new FileReader();
+            reader.onload = function(e) {
+                let fileContent = e.target.result;
+                // Stocker l'image dans le local storage sous le nom de image
+                localStorage.setItem('image', fileContent);
+            };
+            reader.readAsDataURL(file);
+
             window.location.href = "./select-image.html";
         }
     })
 }
 
+/** Affiche l'image stockée dans le local storage */
 function displaySelectedImage() {
-    console.log(selectedImage);
+    let image = document.getElementById('imgSelected');
+    image.src = localStorage.getItem('image');
+    console.log(localStorage.getItem('image'));
+}
+
+/** envoie l'image choisie */
+async function makeGuess() {
+    let image = localStorage.getItem('image');
+    let byteString = atob(image.split(',')[1]);
+    let imageType = image.split(',')[0].split(':')[1].split(';')[0];
+    let blob = new Blob([new ArrayBuffer(atob(image.split(',')[1]).length)], {type: imageType});
+    
+    let formData = new FormData();
+
+    formData.append("guessimage", blob);
+
+    let response = await fetch(apiUrl + "guesses", {
+        method: "POST", 
+        body: formData
+    });
+
+    let responseContent = await response.json();
+    if (responseContent) {
+        console.log(responseContent);
+    } 
 }
